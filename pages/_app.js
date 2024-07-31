8ec9c6f13313dc180b909dc96128121881889bf9
Yogesh Gohil <yogeshgohil@aviansoft.in>
2024-07-31 12:22:04 +0530

    added speed insight package

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
